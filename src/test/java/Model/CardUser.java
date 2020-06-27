f5715f8b036304c1cd58dc6f8f146ab7816f97da
Gurina Elena <gurina_elena@inbox.ru>
2020-06-27 22:42:01 +0300

    Добавление основных файлов

---
+++ src/test/java/Model/CardUser.java
@@ -0,0 +1,13 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CardUser {
+private String city;
+private String fullName;
+private String phoneNumber;
+
+}
